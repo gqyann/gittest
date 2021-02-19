fc4f4781b4adbe093e51aeacf97edb3831afabe5
772734352@qq.com <772734352@qq.com>
2021-02-19 18:54:21 +0800

    master commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("nihao");
         System.out.println("second commit");
+        System.out.println("master");
     }
 }
